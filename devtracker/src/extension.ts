0f19c6bfb1dcf60d9783eb42fb02920dc35e8fd8
dinulawe <Nethan.w12@gmail.com>
2024-06-22 21:16:14 -0400

    Created globalcontext variable to save time

--- devtracker/src/extension.ts
+++ devtracker/src/extension.ts
@@ -4,6 +4,7 @@ import { supportedLanguages } from './languages';
 
 let timer: Timer | null = null
 let currentLanguage: string | undefined
+let global_context: vscode.ExtensionContext 
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "devtracker" is now active!');
@@ -11,6 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Dev Tracker is now tracking!');
 	});
 	
+    //saves context for future use
+    global_context = context;
+
 	context.subscriptions.push(disposable);
 
 	vscode.workspace.onDidOpenTextDocument((document) => {
@@ -40,12 +44,24 @@ function startTimer(languageId: string) {
         console.log(`Current Language is ${languageId}`);
         timer = new Timer();
         timer.start();
+
+        // Prints the time spent on project during the begining of the project
+        let time_worked = global_context.globalState.get("Current_Time", 0);
+        console.log(`Timer has started, the current time worked is ${time_worked} seconds`);
     }
 }
 
 function stopTimer() {
     if (timer) {
+        // Gets the current time before the time is stopped
+        let current_time = timer.getElapsedTimeInSeconds()
+
         timer.stop();
         timer = null;
+
+        // Adds the previous time to the current time
+        let time_added = global_context.globalState.get("Current_Time", 0) + current_time;
+        global_context.globalState.update("Current_Time", time_added);
+        console.log(`Timer is stopped, the current time worked is ${time_added} seconds`);
     }
 }
