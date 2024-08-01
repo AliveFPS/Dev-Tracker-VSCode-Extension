bf32d681d5e43cf3b016a70bba8cf9b4ea763af4
dinulawe <Nethan.w12@gmail.com>
2024-08-01 14:12:27 +0530

    created writeFile function

---
+++ devtracker/src/writeData.ts
@@ -0,0 +1,20 @@
+import { getLanguageDataPath } from "./readData";
+import { readFile } from "./readData";
+import * as fs from 'fs';
+
+export function writeFile(language: string, time: number): void{
+    let data = readFile();
+    let dictionary = data['languages'];
+    // Checking to see if the language exists in the object, if not add the language and the time
+    if (dictionary[language] !== undefined){
+        dictionary[language] = dictionary[language] + time;
+    }
+    else{
+        dictionary[language] = time;
+    }
+    //Adds the dictionary into the "languages" key and writes to file
+    let filePath = getLanguageDataPath();
+    data['languages'] = dictionary
+    fs.writeFileSync(filePath,JSON.stringify(data, null, 2))
+}
+
