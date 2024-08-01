77ef8c5a776e3df11adc73258cf1deaa17fedea4
dinulawe <Nethan.w12@gmail.com>
2024-08-01 15:26:47 +0530

    fixed property error

--- devtracker/src/writeData.ts
+++ devtracker/src/writeData.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 
 export function writeFile(language: string, time: number): void{
     let data = readFile();
-    let dictionary = data['languages'];
+    let dictionary = data.languages;
     // Checking to see if the language exists in the object, if not add the language and the time
     if (dictionary[language] !== undefined){
         dictionary[language] = dictionary[language] + time;
@@ -14,7 +14,7 @@ export function writeFile(language: string, time: number): void{
     }
     //Adds the dictionary into the "languages" key and writes to file
     let filePath = getLanguageDataPath();
-    data['languages'] = dictionary
+    data.languages = dictionary
     fs.writeFileSync(filePath,JSON.stringify(data, null, 2))
 }
 
